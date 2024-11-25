e14c24fb1809ee6735b20564721a201b37aff7df
emilym1106 <ealt11@terpmail.umd.edu>
2024-11-25 14:00:52 -0500

    created scene transition script

--- Assets/PlayerScripts/Scripts/Collide Trigger.cs
+++ Assets/PlayerScripts/Scripts/Collide Trigger.cs
@@ -5,14 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class CollideTrigger : MonoBehaviour
 {
+    [SerializeField] Animator transitionAnim;
     private void OnTriggerEnter(Collider other)
     {
-        
-            Debug.Log(SceneManager.GetActiveScene().name);
-            // Change scene
-            SceneManager.LoadScene("BeeScene");
-        
-      
-      
+
+        Debug.Log(SceneManager.GetActiveScene().name);
+        {
+            StartCoroutine(LoadLevel());
+            IEnumerator LoadLevel()
+            {
+                transitionAnim.SetTrigger("End");
+                yield return new WaitForSeconds(1);
+                SceneManager.LoadScene("BeeScene");
+                transitionAnim.SetTrigger("Start");
+
+            }
+
+
+
+
+
+        }
+    
     }
 }
